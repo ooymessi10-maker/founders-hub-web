13f10df8fc2c9e1ed61988a2c121ba25a1655f9a
ooymessi10 <ooymessi10@gmail.com>
2026-07-11 00:06:48 +0700

    remove Date prototype getWeek

--- app/vote/page.tsx
+++ app/vote/page.tsx
@@ -68,11 +68,5 @@ const currentWeek = Math.ceil((now.getDay() + 1 + days) / 7);
   );
 }
 
-// เสริมฟังก์ชันเช็คสัปดาห์ของปี
-Date.prototype.getWeek = function() {
-  var date = new Date(this.getTime());
-  date.setHours(0, 0, 0, 0);
-  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-  var week1 = new Date(date.getFullYear(), 0, 4);
-  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+
 };
